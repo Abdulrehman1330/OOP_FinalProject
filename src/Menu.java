43ba90cbb5146f65123939ea7bb80a3b968c9d30
Abdulrehman1330 <abdulrehmanashfaq.aa@gmail.com>
2023-11-13 17:10:12 +0500

    First Commit

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    private int no;
+
+    public void showOptions(){
+        System.out.println("Choose an option from following");
+        System.out.println("1. Login");
+        System.out.println("2. SignUp");
+        Scanner scanner = new Scanner(System.in);
+        no=scanner.nextInt();
+        switch (no){
+            case 1:
+                break;
+            case 2:
+                Showroom.addNewUser();
+        }
+    }
+
 
 }

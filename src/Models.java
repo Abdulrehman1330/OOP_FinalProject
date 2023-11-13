a9d185fd77e340cd1a78bb9a02307f7a1a4c441f
Ayesh <ayeshahmad2003@gmail.com>
2023-11-13 16:18:20 +0500

    commit

--- src/Models.java
+++ src/Models.java
@@ -1,10 +1,11 @@
 public enum Models {
-    COROLLA(5,"Corolla"),YARIS(5,"Yaris"),SUPRA(2,"Supra"),FORTUNER(7,"Fortuner"),LANDCRUISER(7,"Land Cruiser"),PRIUS(5,"Prius");
+    COROLLA(5,"Corolla", "2NR-FE"),YARIS(5,"Yaris", "XP-150"),SUPRA(2,"Supra", "B48"),FORTUNER(7,"Fortuner", "2TR-FE"),LANDCRUISER(7,"Land Cruiser", "1VD-FTV"),PRIUS(5,"Prius", "2ZR-FXE");
 
     private int seats;
 
     private String name;
     private String engine;
+    private
     Models(int seat, String name, String engine){
         this.seats = seat;
         this.name = name;
@@ -13,6 +14,8 @@ public enum Models {
 
     @Override
     public String toString() {
-        return "Toyota " + name;
+        return "Toyota " + name + '\n' +
+                " Engine name " + engine + '\n' +
+                " Engine Size " + ;
     }
 }

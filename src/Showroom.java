77cbb217fb5a1da931e6d95063050e2acbe261cd
Abdulrehman1330 <abdulrehmanashfaq.aa@gmail.com>
2023-11-13 16:31:50 +0500

    First Commit

--- src/Showroom.java
+++ src/Showroom.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Showroom {
-    Models cars;
-    String users;
+    static ArrayList<User> users = new ArrayList<>();
+    ArrayList<Models> cars = new ArrayList<>();
+
+    public static void addNewUser(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Enter a new username:");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+        users.add(new User(username,password,name));
+        System.out.println(users.get(0));
+    }
+
 }
--- src/User.java
+++ src/User.java
@@ -44,4 +44,8 @@ public class User {
         }
     }
 
+    @Override
+    public String toString() {
+        return username+ " " + password + " " + name;
+    }
 }

486820e42d6cb06f4a4558dfc75f16f0379b88c8
Abdulrehman1330 <abdulrehmanashfaq.aa@gmail.com>
2023-11-13 16:03:53 +0500

    First Commit

--- src/Models.java
+++ src/Models.java
@@ -1,10 +1,16 @@
 public enum Models {
-    COROLLA(5),YARIS(5),SUPRA(2),FORTUNER(7),LANDCRUISER(7),PRIUS(5);
+    COROLLA(5,"Corolla"),YARIS(5,"Yaris"),SUPRA(2,"Supra"),FORTUNER(7,"Fortuner"),LANDCRUISER(7,"Land Cruiser"),PRIUS(5,"Prius");
 
     private int seats;
 
     private String name;
-    Models(int seat, String ){
+    Models(int seat, String name){
         this.seats = seat;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Toyota " + name;
     }
 }
